9a00095ed8c7cc1a428c1d39df74b677465a7d03
Miguel Vizcaino <miguel87831@gmail.com>
2023-09-18 11:01:56 -0500

    fix layout problem

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -4,7 +4,7 @@ const PrivateLayout = ({
     children: React.ReactNode;
   }) => {
     return (
-      <main className="bg-[#111827] overflow-auto">
+      <main className="bg-[#111827] overflow-auto !min-h-screen">
         <div className="mx-auto max-w-screen-xl h-full w-full">
           {children}
         </div>
